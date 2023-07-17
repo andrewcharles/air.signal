a81f10528f7576b0b3401b04643a1c7b69a73fb8
andrew charles <charlesan@cdmsmith.com>
2023-07-17 12:11:07 +1000

    reorganisation

--- collect/earth_engine_rest.py
+++
@@ -1,5 +0,0 @@
-from google.oauth2 import service_account
-credentials = service_account.Credentials.from_service_account_file(os.getenv('EE_KEY'))
-scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-from google.auth.transport.requests import AuthorizedSession
-session = AuthorizedSession(scoped_credentials)
